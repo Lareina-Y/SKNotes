b72477c9968ce09a46b6713fe6f217495cb1d8f9
”Shiwen <fairy3219889700@gmail.com>
2024-06-05 23:55:09 -0300

    search notes

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { SafeAreaView, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { TouchableOpacity, View, Text , TextInput, } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import 'react-native-reanimated'; 
+import { useAddNoteMutation, useSearchNotesQuery } from './db';
+import { PaperProvider } from 'react-native-paper';
+import MasonryList from '@react-native-seoul/masonry-list';
 
-function App() {
+function HomeScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const [ addNote ] = useAddNoteMutation();
+  const { data: notes, refetch } = useSearchNotesQuery(searchQuery);
+
+  const handleAddNote = async () => {
+    const newNote = {
+      title: 'New Note',
+      content: 'This is a new note'
+    };
+    await addNote(newNote);
+    refetch();
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={[tw`bg-gray-800 p-2 m-1`]}>
+      <Text style={[tw`text-5l text-white`]}>
+        {item.title}
+      </Text>
+      <Text style={[tw`text-1xl text-white`]}>
+        {item.content}
+      </Text>
+    </View>
+  );
+
+  return (
+    <PaperProvider>
+      <View style={tw`flex-1 bg-black text-white pt-15`}>
+        <Text style={tw`text-white mx-auto text-3xl mb-2 font-bold`}>Notes</Text>
+        <TextInput
+          style={tw`p-3 ml-4 mr-4 mb-2 rounded text-white bg-gray-800`}
+          placeholder="Search..."
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+        <MasonryList
+          style={tw`ml-3 mr-3`}
+          data={notes || []}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          renderItem={renderItem}
+          onEndReachedThreshold={0.1}
+        />
+        <TouchableOpacity onPress={handleAddNote} style={tw`bg-blue-500 rounded-full absolute bottom-[5%] right-8 mx-auto items-center flex-1 justify-center w-12 h-12`}>
+          <Text style={tw`text-white text-center text-3xl mt--1`}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </PaperProvider>
+  );
+}
+
+export default function App() {
   useDeviceContext(tw);
 
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <Text style={tw`w-screen mt-16 text-center text-xl`}>
-          Your app code goes here.
-        </Text>
-      </SafeAreaView>
+      <HomeScreen />
     </Provider>
-  )
+  );
 }
-
-export default App;
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# SKNotesStarter
+# SKNotes
 
 View the [ShiftKey Labs Notion](https://shiftkeylabs.notion.site/Project-Install-Instructions-f937641104bc42e098fcfefcf7349608) for detailed installation instructions.
